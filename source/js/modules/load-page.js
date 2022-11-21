b79c637aed7cca2611e30b7409c630fac1ef70ef
«Антон» <nirvs@mail.ru>
2022-11-21 23:51:12 +0300

    добавили анимацию заголовков

--- source/js/modules/load-page.js
+++ source/js/modules/load-page.js
@@ -1,18 +1,18 @@
 export default () => {
-    window.onload = function(){
-        const body = document.body;
-        const menuLinks = body.querySelectorAll(`.js-menu-link`);
+  window.onload = function () {
+    const body = document.body;
+    const menuLinks = body.querySelectorAll(`.js-menu-link`);
 
-        if(!body.classList.contains(`loaded`)) {
-            body.classList.add(`loaded`);
-        }
-
-        menuLinks.forEach((link) => {
-            link.classList.remove(`active`);
-        });
-
-        setTimeout(() => {
-            menuLinks[0].classList.add(`active`);
-        }, 350);
+    if (!body.classList.contains(`loaded`)) {
+      body.classList.add(`loaded`);
     }
+
+    menuLinks.forEach((link) => {
+      link.classList.remove(`active`);
+    });
+
+    setTimeout(() => {
+      menuLinks[0].classList.add(`active`);
+    }, 350);
+  };
 };
